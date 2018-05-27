b0a8f4e00fb69b0b8ec4c958f5ea75efbb891e4e
AndresChac <a.chacon@cognitiva.la>
2018-05-27 13:22:38 -0400

    Creacion de servlet para nuevo cliente.

--- LindaSonrisa/src/java/SERVLET/nuevoCliente.java
+++ LindaSonrisa/src/java/SERVLET/nuevoCliente.java
@@ -7,18 +7,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import DAO_IMP.ClienteDaoImp;
-import DTO.ClienteDto;
-
-import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  *
  * @author andres
  */
-@WebServlet(name = "loginConRut", urlPatterns = {"/loginConRut"})
-public class loginConRut extends HttpServlet {
+@WebServlet(name = "nuevoCliente", urlPatterns = {"/nuevoCliente"})
+public class nuevoCliente extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -34,40 +29,16 @@ public class loginConRut extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            //abrimos una sesion
-            HttpSession session = request.getSession();
-            //cliente aux
-            if (session.getAttribute("cliente") != null) {
-                ClienteDto aux = (ClienteDto) session.getAttribute("cliente");
-                //osea que ya estamos recibiendo contraseña
-                //crearemos un atributo para informar que el logeo se hiso de forma correcta
-                String pass = request.getParameter("txtPass");
-                //ciframos contraseña del formulario y la comprobamos con la acutal
-                if (aux.getContrasenia().equals(DigestUtils.md5Hex(pass))) {
-                    //ahora se debera comprobar si este atributo existe en la sesion
-                    session.setAttribute("acceso", 1);
-                    response.sendRedirect("PAGES/AgendarHora.jsp");
-                } else {
-                    session.setAttribute("acceso", 0);
-                    response.sendRedirect("PAGES/IngresarRut.jsp");
-                }
-            } else {
-                ClienteDto clienteAux = new ClienteDto();
-                clienteAux.setRut(request.getParameter("txtRut"));
-                clienteAux.setHabilitado(false);
-                clienteAux = new ClienteDaoImp().buscar(clienteAux);
-                if (clienteAux.isHabilitado()) {
-                    session.setAttribute("cliente", clienteAux);
-                    response.sendRedirect("PAGES/IngresarRut.jsp");
-                } else {
-                    response.sendRedirect("PAGES/RegistroCliente.jsp");
-                }
-//                //probando despliege de rut
-//                clienteAux.setContrasenia("132");
-//                session.setAttribute("cliente", clienteAux);
-//                response.sendRedirect("PAGES/IngresarRut.jsp");
-            }
-
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet nuevoCliente</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet nuevoCliente at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
     }
 
---
+++ LindaSonrisa/web/PAGES/RegistroCliente.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : RegistroCliente
+    Created on : 27-05-2018, 13:14:00
+    Author     : andres
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Registro nuevo cliente</title>
+    </head>
+    <body>
+        <h1>Hello! tus datos:</h1>
+        <form action="LindaSonrisa/nuevoCliente" method="POST">
+            <span>Formulario para ingreso nuevo cliente</span>
+        </form>
+    </body>
+</html>

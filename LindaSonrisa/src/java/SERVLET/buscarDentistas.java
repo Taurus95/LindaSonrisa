4d631461d794f753d0e7b660ec1cfb9b748cb0cc
AndresChac <a.chacon@cognitiva.la>
2018-05-27 14:09:12 -0400

    servlet buscar dentitas para especialidad

---
+++ LindaSonrisa/src/java/SERVLET/buscarDentistas.java
@@ -0,0 +1,88 @@
+package SERVLET;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import DTO.TrabajadorDto;
+import DAO_IMP.TrabajadorDaoImp;
+import java.util.ArrayList;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author andres
+ */
+@WebServlet(name = "buscarDentistas", urlPatterns = {"/buscarDentistas"})
+public class buscarDentistas extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            HttpSession session = request.getSession();
+            if ((int) session.getAttribute("acceso") == 1) {
+                String especialidad = request.getParameter("especialidad");
+                ArrayList<TrabajadorDto> lista = (ArrayList<TrabajadorDto>) new TrabajadorDaoImp().listarDentistaEspecialidad(especialidad);
+                session.setAttribute("especialidad", especialidad);
+                session.setAttribute("dentistas", lista);
+                response.sendRedirect("PAGES/EspecialidadDoctor.jsp");
+            } else {
+                response.sendRedirect("PAGES/Home.jsp");
+            }
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}

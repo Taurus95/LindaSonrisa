135dc81557d9eb0fd9b4e27bc82fc3d57cd994f6
aandres <aandres@aandres-PC>
2018-07-09 23:28:10 -0400

    funcionando

---
+++ LindaSonrisa/src/java/SERVLET/verDetalle.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SERVLET;
+
+import DAO_IMP.ConsultaDaoImp;
+import DTO.ConsultaDto;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author aandres
+ */
+@WebServlet(name = "verDetalle", urlPatterns = {"/verDetalle"})
+public class verDetalle extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            HttpSession session = request.getSession();
+            if (session.getAttribute("acceso") == null || (int) session.getAttribute("acceso") != 1) {
+                session.invalidate();
+                response.sendRedirect("index.html");
+                return;
+            }
+            int id = Integer.parseInt(request.getParameter("idConsulta"));
+            ConsultaDto consulta = new ConsultaDto();
+            consulta.setIdConsulta(id);
+            ConsultaDaoImp implement = new ConsultaDaoImp();
+            consulta = implement.buscar(consulta);
+            session.setAttribute("consulta", consulta);
+            response.sendRedirect("PAGES/DetalleConsultaSecretaria.jsp");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
